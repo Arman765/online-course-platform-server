7e730860901b3fa9baa869964eea57aafc8d01ab
Arman765 <friend.of.laden765@gmail.com>
2022-10-25 12:47:05 +0600

    api

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+
 const courses = require("./data/courses.json");
+const course_details = require("./data/course_details.json");
 
 app.get("/", (req, res) => {
   res.send("Server API is running ");
